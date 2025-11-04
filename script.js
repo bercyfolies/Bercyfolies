7174b21c121198a90dd5d870b6b212392bb96f16
Laurent <promeliak2018@proton.me>
2025-11-04 01:06:02 +0100

    connexion à la bdd

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function togglePassword() {
         passwordInput.type = 'password';
     }
 }
+function validateLogin() {
+    const telephone = document.getElementById('telephone').value;
+    const password = document.getElementById('password').value;
+
+    // Requête AJAX pour vérifier le couple téléphone/mot de passe dans la base de données
+    fetch('http://176.185.182.77/login.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ telephone: telephone, password: password }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.valid) {
+            alert('Connexion réussie !');
+            closePopup();
+        } else {
+            alert('Numéro de téléphone ou mot de passe incorrect.');
+        }
+    })
+    .catch((error) => {
+        console.error('Erreur:', error);
+        alert('Une erreur est survenue lors de la connexion.');
+    });
+}
